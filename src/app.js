bcc81538e01fc35f21d73c37edfdba7d16d85497
Dhanush Kumar AN <160763637+dhanushkumar25@users.noreply.github.com>
2025-08-24 11:35:20 +0530

    Played with routes like /abc etc

--- src/app.js
+++ src/app.js
@@ -3,24 +3,8 @@ const express = require("express");
 const app = express();
 
 // This will only handle GET call to /user
-app.get("/user",(req,res) => {
+app.get("/abc",(req,res) => {
     res.send({ firstName : "Dhanush", lastName : "Kumar"});
-})
-
-app.post("/user",(req,res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the database!");
-})
-
-app.delete("/user",(req,res) => {
-    res.send("Data deleted successfully");
-})
-
-
-
-
-app.use("/test",(req,res) => {
-    res.send("Hello from the server!");
 })
 
 app.listen(7777, ()  => {
