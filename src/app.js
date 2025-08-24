d951b99c9d54815b3398078932c26ea3354419fa
Dhanush Kumar AN <160763637+dhanushkumar25@users.noreply.github.com>
2025-08-24 11:21:33 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/Hello",(req,res) => {
-    res.send("Hello hello !");
+// This will only handle GET call to /user
+app.get("/user",(req,res) => {
+    res.send({ firstName : "Dhanush", lastName : "Kumar"});
 })
+
+app.post("/user",(req,res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the database!");
+})
+
+app.delete("/user",(req,res) => {
+    res.send("Data deleted successfully");
+})
+
+
+
+
 app.use("/test",(req,res) => {
     res.send("Hello from the server!");
 })
