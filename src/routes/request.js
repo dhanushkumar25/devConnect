d0b62021a32dc490e08eb9c0c8b849f5172f35f6
Dhanush Kumar AN <160763637+dhanushkumar25@users.noreply.github.com>
2025-10-06 10:59:06 +0530

    "Wrote APIs for accepting/rejecting requests"

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,7 +3,7 @@ const requestRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-const User = require("../models/user")
+const User = require("../models/user");
 requestRouter.post(
   "/request/send/:status/:toUserId",
   userAuth,
@@ -19,10 +19,10 @@ requestRouter.post(
           .status(400)
           .json({ message: "Invalid status type: " + status });
       }
-      
+
       // To check if user exists
       const toUser = await User.findById(toUserId);
-      if(!toUser) {
+      if (!toUser) {
         return res.status(404).json({ message: "User not found" });
       }
 
@@ -48,12 +48,53 @@ requestRouter.post(
       const data = await connectionRequest.save();
 
       res.json({
-        message: req.user.firstName + " is " + status + " in " + toUser.firstName,
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
         data,
       });
     } catch (err) {
       res.status(400).send("ERROR: " + err.message);
     }
+  }
+);
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+      // Dhanush => Elon
+      // loggedInId == toUserId 
+      // status = interested
+      // request id should be valid
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId} = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if(!allowedStatus.includes(status)){
+        return res.status(400).json({message: "status not allowed!"});
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId:loggedInUser._id,
+        status: "interested",
+      })
+
+      if(!connectionRequest){
+        return res.status(404).json({message: "Conncetion request not found"});
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+      res.json({message: "conncetion request " + status, data});
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);      
+    }
+
+
+
   }
 );
 
